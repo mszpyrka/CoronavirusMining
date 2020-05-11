becfa873f667ca67b78d77758e4812efa7439e63
jano <kuba.jano3@gmail.com>
2020-05-11 20:49:20 +0200

    Add processing script with followers

---
+++ processing/process-with-followers.py
@@ -0,0 +1,92 @@
+import os
+import json
+import pandas as pd
+from collections import defaultdict
+import shutil
+
+directory = './input'
+directory_out = './output'
+
+parts = 4
+
+
+class CountryDict:
+    def __init__(self):
+        translation_file = '../analysis-utils/countries-translation/global_names.csv'
+        self.translation_df = pd.read_csv(translation_file, keep_default_na=False, na_values=[''])
+        self.country_list = []
+        for country in self.translation_df['alpha_2'].tolist():
+            if country:
+                self.country_list.append(country)
+
+        self.country_dict = {k: [] for k in self.country_list}
+        self.country_percent = {k: {} for k in self.country_list}
+
+    def clear(self):
+        self.country_dict = defaultdict(list)
+
+    def append(self, key, value):
+        self.country_dict[key].append(value)
+
+    def store_percent(self):
+        for key in self.country_dict:
+            country_max = self.country_dict[key][0]
+            self.country_percent[key]['max'] = country_max
+            step = country_max/parts
+            threshold = country_max - step
+            counter = 0
+            part_number = 1
+            #print("Start threshold %s" % threshold)
+            for fol_count in self.country_dict[key]:
+                processed = False
+                #print("Actual threshold %s and part number %s and value %s" % (threshold, part_number, fol_count))
+                while not processed:
+                    if fol_count >= threshold:
+                        counter += 1
+                        processed = True
+                    else:
+                        #print("Stored " + key + " " + str(1 - part_number/parts) + " " + filename)
+                        percent_step = (1 - part_number/parts) * 100
+                        self.country_percent[key][str(percent_step) + '%+'] = counter
+                        counter = 0
+                        part_number += 1
+                        #print(part_number)
+                        threshold -= step
+
+            percent_step = (1 - part_number / parts) * 100
+            self.country_percent[key][str(percent_step) + '%+'] = counter
+
+        with open(directory_out + '/' + filename[:-5] + '-percent.json', 'w') as fp:
+            json.dump(self.country_percent, fp)
+
+    def store(self, filename):
+        for key in self.country_dict:
+            self.country_dict[key].sort(reverse=True)
+
+        with open(directory_out + '/' + filename, 'w') as fp:
+            json.dump(self.country_dict, fp)
+
+        self.store_percent()
+
+
+def process_json(filename, country_dict):
+    with open(directory + '/' + filename) as json_file:
+        data = json.load(json_file)
+        country_dict.clear()
+        for record in data['records']:
+            country_dict.append(record['country'], record['followers_count'])
+
+        #country_dict.append(data['records'][0]['country'], data['records'][0]['followers_count'])
+        country_dict.store(filename)
+
+
+if __name__ == "__main__":
+    shutil.rmtree('./output', ignore_errors=True)
+
+    if not os.path.exists('./output'):
+        os.makedirs('./output')
+
+    country_dict = CountryDict()
+    for filename in os.listdir(directory):
+        print("Process %s" % filename)
+        process_json(filename, country_dict)
